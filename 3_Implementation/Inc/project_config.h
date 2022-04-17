aa9f582a9a8649383945ffc09bc61fbed4fca58c
samanvitha-125 <62429376+samanvitha-125@users.noreply.github.com>
2022-04-17 08:11:12 +0530

    Create project_config.h

---
+++ 3_Implementation/Inc/project_config.h
@@ -0,0 +1,23 @@
+#ifndef __PROJECT_CONFIG_H__
+#define __PROJECT_CONFIG_H__
+/**
+ * @file project_config.h
+ * @author Samanvitha Reddy Padhuri
+ * @brief Configuration file to define pins and ports for the interfaced peripherals
+ * @version 0.1
+ * @date 2022-04-17
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#define MODE_SELECT      0 
+#define MANUAL_SWITCH    1
+#define MOTOR_STATE      8
+#define SENSOR_1         9
+#define SENSOR_2         10
+#define SENSOR_3         11
+#define SENSOR_4         12
+#define SENSOR_5         13
+
+
+#endif /* __PROJECT_CONFIG_H__ */

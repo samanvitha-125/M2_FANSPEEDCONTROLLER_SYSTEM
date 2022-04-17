0b9b5c681957a5eb7916d478ef2c8eaaf3a3f97c
samanvitha-125 <62429376+samanvitha-125@users.noreply.github.com>
2022-04-17 08:13:39 +0530

    Create user_utils.h

---
+++ 3_Implementation/Inc/user_utils.h
@@ -0,0 +1,19 @@
+#ifndef __USER_UTILS_H__
+#define __USER_UTILS_H__
+/**
+ * @file user_utils.h
+ * @author Samanvitha Reddy Padhuri
+ * @brief User defined utilities used across the project
+ * @version 0.1
+ * @date 2022-04-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "project_config.h"
+#include <util/liquidCrystal.h>
+
+
+
+
+#endif /* __USER_UTILS_H__ */
